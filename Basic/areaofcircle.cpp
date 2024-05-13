d61d1b3e16726f51bf13a91d1f1e4ab6755aca03
rojinatiwari <rojinatiwari17@gmail.com>
2024-05-13 20:34:13 +0545

    area

---
+++ Basic/areaofcircle.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class circle
+{
+ private:
+    float rad,area ;
+
+    public:
+    void calculate()
+    {
+        cout<<"Enter the radius of the circle";
+        cin>>rad;
+        area=3.14*rad*rad;
+        cout<<"circle="<<area;
+    }
+};
+
+int main()
+{
+    circle obj;
+    obj.calculate();
+    return 0;
+}
+
+   
+    
