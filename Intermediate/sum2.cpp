b7ff5b6ad591bcdc4095cce92429c9c55e52cfab
rojinatiwari <rojinatiwari17@gmail.com>
2024-06-24 19:23:35 +0545

    class template using multiple parameterized type

---
+++ Intermediate/sum2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+template<class T1, class T2>
+
+class sum
+{
+    private:
+    T1 a;
+    T2 b;
+
+    public:
+    void input(T1 x, T2 y)
+    {
+        a=x;
+        b=y;
+    }
+
+
+    void display()
+    {
+        cout<<"sum ="<<(a+b)<<endl;
+    }
+
+    
+};
+
+int main() {
+    sum <int,float> s1;
+    sum<float, int> s2;
+    s1.input(2,4.5);
+    s2.input(3.3,3);
+    s1.display();
+    s2.display();
+    return 0;
+}
